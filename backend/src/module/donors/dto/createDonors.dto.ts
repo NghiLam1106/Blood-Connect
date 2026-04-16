305544aa689d082cfb25120456436604d2f42b72
Lâm Chính Nghi <lam91391@gmail.com>
2026-04-16 11:33:43 +0700

    refactor: implement name and avatar attributes

--- backend/src/module/donors/dto/createDonors.dto.ts
+++ backend/src/module/donors/dto/createDonors.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 import { Status } from "../../../../generated/prisma/enums";
 
 export class CreateDonorsDto {
+  @ApiProperty({ description: 'Tên người hiến máu' })
+  @IsNotEmpty({ message: 'Tên không được để trống' })
+  @IsString({ message: 'Tên phải là chuỗi' })
+  name!: string;
+
   @ApiProperty({ description: 'Cân nặng người hiến máu' })
   @IsNotEmpty({ message: 'Cân nặng không được để trống' })
   @IsNumber({}, { message: 'Cân nặng phải là số' })
@@ -23,15 +28,20 @@ export class CreateDonorsDto {
   @IsString({ message: 'Nhóm máu phải là chuỗi' })
   bloodType!: string;
 
-  @ApiProperty({ description: 'Ngày hiến máu gần nhất' })
-  @IsNotEmpty({ message: 'Ngày hiến máu gần nhất không được để trống' })
-  @IsDate({ message: 'Ngày hiến máu gần nhất phải là ngày' })
-  lastDonation!: Date;
+  // @ApiProperty({ description: 'Ngày hiến máu gần nhất' })
+  // @IsNotEmpty({ message: 'Ngày hiến máu gần nhất không được để trống' })
+  // @IsDate({ message: 'Ngày hiến máu gần nhất phải là ngày' })
+  // lastDonation!: Date;
 
-  @ApiProperty({ description: 'Tỷ lệ phản hồi' })
-  @IsNotEmpty({ message: 'Tỷ lệ phản hồi không được để trống' })
-  @IsNumber({}, { message: 'Tỷ lệ phản hồi phải là số' })
-  responseRate!: number;
+  // @ApiProperty({ description: 'Tỷ lệ phản hồi' })
+  // @IsNotEmpty({ message: 'Tỷ lệ phản hồi không được để trống' })
+  // @IsNumber({}, { message: 'Tỷ lệ phản hồi phải là số' })
+  // responseRate!: number;
+
+  @ApiProperty({ description: 'Ảnh đại diện' })
+  @IsString({ message: 'Ảnh đại diện phải là chuỗi' })
+  @IsNotEmpty({ message: 'Ảnh đại diện không được để trống' })
+  avatar!: string;
 
   @ApiProperty({ description: 'Trạng thái' })
   @IsNotEmpty({ message: 'Trạng thái không được để trống' })
